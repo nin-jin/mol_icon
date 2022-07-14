b9064c08d06aebe55f0c24bf625a31243999d2ed
nin-jin <nin-jin@users.noreply.github.com>
2022-07-14 07:21:42 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.js
+++ node.js
@@ -52428,6 +52428,9 @@ var $;
         enabled() {
             return true;
         }
+        bring() {
+            return this.Query().bring();
+        }
         Query() {
             const obj = new this.$.$mol_string();
             obj.value = (val) => this.query(val);
