94492a6139c80ffcac28aa9320ca15c8052027b1
nin-jin <nin-jin@users.noreply.github.com>
2022-09-17 07:23:32 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.js
+++ web.js
@@ -51190,7 +51190,10 @@ var $;
         selection(val) {
             if (val !== undefined)
                 return val;
-            return [];
+            return [
+                0,
+                0
+            ];
         }
         auto() {
             return [
@@ -51206,7 +51209,8 @@ var $;
                 spellcheck: this.spellcheck(),
                 autocomplete: this.autocomplete_native(),
                 selectionEnd: this.selection_end(),
-                selectionStart: this.selection_start()
+                selectionStart: this.selection_start(),
+                inputMode: this.keyboard()
             };
         }
         attr() {
@@ -51259,6 +51263,9 @@ var $;
         }
         selection_start() {
             return 0;
+        }
+        keyboard() {
+            return "text";
         }
         length_max() {
             return Infinity;
@@ -51332,7 +51339,7 @@ var $;
             event_change(next) {
                 if (!next)
                     return;
-                this.value(next.target.value);
+                this.value_changed(next.target.value);
                 this.selection_change(next);
             }
             hint_visible() {
@@ -51351,10 +51358,12 @@ var $;
                 const el = this.dom_node();
                 if (el !== this.$.$mol_dom_context.document.activeElement)
                     return;
-                this.selection([
+                const [from, to] = this.selection([
                     el.selectionStart,
                     el.selectionEnd,
                 ]);
+                el.selectionEnd = to;
+                el.selectionStart = from;
             }
             selection_start() {
                 return this.selection()[0];
